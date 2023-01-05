b933cc3bbbc0dc15ca9fc531c8079b0d57b18251
Mertcan Taşkıran <mertcan12furkan@gmail.com>
2023-01-05 18:02:26 +0300

    FlexBoxPhotoGallery
    Flex Box Photo Gallery

---
+++ script.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => { 
+    item.addEventListener('click', () => {
+        items.forEach(item => item.classList.remove('show'));
+        item.classList.add('show');
+    });   
+});
